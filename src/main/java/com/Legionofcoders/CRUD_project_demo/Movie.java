c31ad9d1b30be10bd91205b9061dc2e09b998974
santhosh <santhosh29.dev@gmail.com>
2026-02-16 19:08:43 +0530

    Movie Endpoints

---
+++ src/main/java/com/Legionofcoders/CRUD_project_demo/Movie.java
@@ -0,0 +1,35 @@
+package com.Legionofcoders.CRUD_project_demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imdbid;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
+}
