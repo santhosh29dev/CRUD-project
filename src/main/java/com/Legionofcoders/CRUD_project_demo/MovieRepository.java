995a6e5ef5783d1e5d9a351c3e0422c67ae5a3de
santhosh <santhosh29.dev@gmail.com>
2026-02-16 21:14:46 +0530

    feat(movie): implement REST API endpoints
    Add CRUD operations for Movie resource including
    create, fetch, update, and delete functionality.

---
+++ src/main/java/com/Legionofcoders/CRUD_project_demo/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.Legionofcoders.CRUD_project_demo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie , ObjectId> {
+
+
+}
---
+++ src/main/java/com/Legionofcoders/CRUD_project_demo/Reviews.java
@@ -0,0 +1,20 @@
+package com.Legionofcoders.CRUD_project_demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.bind.annotation.RestController;
+
+@Document(collection = "Reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reviews {
+
+    private ObjectId id;
+    private String body;
+
+}
